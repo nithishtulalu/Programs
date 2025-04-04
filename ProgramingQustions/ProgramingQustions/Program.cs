55a22e83ebea9f092da7d308e2b305d7000a11d9
nithish <nithishtulalu121@gmail.com>
2025-04-04 22:35:39 +0530

    git commit -m "first commit"

---
+++ ProgramingQustions/ProgramingQustions/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ProgramingQustions
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ples enter Number1:");
+            int x= int.Parse(Console.ReadLine());
+            Console.WriteLine("ples enter Number2:");
+            int y= int.Parse(Console.ReadLine());
+            Console.WriteLine($"Number 1:{x} Number 2 :{y}");
+        }
+    }
+}
